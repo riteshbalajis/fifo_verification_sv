26bb5f3078673ebaaf229e876eff3b3d6d985838
riteshbalajis <158824919+riteshbalajis@users.noreply.github.com>
2025-07-09 22:15:27 +0530

    Create fifopkg.sv

---
+++ tb/fifopkg.sv
@@ -0,0 +1,14 @@
+package fifopkg;
+
+  `include "rstgen.sv"
+  `include "write_readtest.sv"
+  `include "underflow_test.sv"
+  `include "overflow_test.sv"
+  `include "empty_test.sv"
+  `include "full_test.sv"
+  `include "result.sv"
+  `include "reset_test.sv"
+  `include "runtest.sv"
+  
+endpackage
+
